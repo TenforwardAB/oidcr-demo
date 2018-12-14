b1418f8b4734cc7fc2d191da40ac9040c7c9fda9
Andre Karlsson <andre@sess.se>
2018-12-14 15:47:03 +0100

    Updating Gitignore and removing some cahce files and IDE stuff from repo

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 
-import uuid
-
 
 class Base:
 	SECRET_KEY = "JEKJKljkj3948kjd892d9j0co"
@@ -16,70 +14,16 @@ class Base:
 	OIDC_SCOPES = ['openid', 'email', 'address']
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
 	VERIFY_SSL = False
 
-	POP_KEYS = [
-		{"type": "RSA", "key": "keys/key.pem", "use": ["sig"]}
-	]
 
-	# information used when registering the client,
-	# this may be the same for all OPs
+class Production(Base):
+	pass
 
-	ME = {
-		"application_type": "web",
-		"application_name": "idpproxy",
-		"contacts": ["ops@example.com"],
-		"redirect_uris": ["{base}authz_cb"],
-		"post_logout_redirect_uris": ["{base}logout_success"],
-		"response_types": ["code"]
-	}
 
-	BEHAVIOUR = {
-		"response_type": "code",
-		"scope": ["openid", "profile", "email", "address", "phone"],
-	}
+class Testing(Base):
+	pass
 
-	# ACR_VALUES = ["SAML"]
-	ACR_VALUES = None
 
-	# The keys in this dictionary are the OPs short userfriendly name
-	# not the issuer (iss) name.
-
-	CLIENTS = {
-		# The ones that support webfinger, OP discovery and client registration
-		# This is the default, any client that is not listed here is expected to
-		# support dynamic discovery and registration.
-		"": {
-			"client_info": ME,
-			"behaviour": BEHAVIOUR
-		},
-		# "oictest": {
-		#    "srv_discovery_url": "https://oictest.umdc.umu.se:8085/",
-		#    "client_info": ME,
-		#    "behaviour": BEHAVIOUR
-		# },
-		# "lingon": {
-		#     "srv_discovery_url": "https://lingon.ladok.umu.se:8092/",
-		#     "client_info": ME,
-		#     "behaviour": BEHAVIOUR,
-		#     "verify_ssl": False
-		# },
-		# Supports OP information lookup but not client registration
-		# "google": {
-		#     "srv_discovery_url": "https://accounts.google.com/",
-		#     "client_registration": {
-		#         "client_id": "xxxxxxxxx.apps.googleusercontent.com",
-		#         "client_secret": "2222222222",
-		#         "redirect_uris": ["{base}google"],
-		#     },
-		#     "behaviour": {
-		#         "response_type": "code",
-		#         "scope": ["openid", "profile", "email"]
-		#     },
-		#     "allow": {
-		#         "issuer_mismatch": True
-		#     },
-		#     "userinfo_request_method": "GET"
-		# }
-	}
+class Development(Base):
+	pass
